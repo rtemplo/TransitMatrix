e8833f810fd7b8ceea408738db71aad92dcdf706
Ray Templo <rtemplo@gmail.com>
2017-09-13 22:38:12 -0400

    directory struct init

---
+++ assets/scripts/script.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+
+$(document).ready(function () {
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyBEz6fWOoz4MWVrhs2qOFjyJuj8FUsyo7U",
+		authDomain: "transit-matrix.firebaseapp.com",
+		databaseURL: "https://transit-matrix.firebaseio.com",
+		projectId: "transit-matrix",
+		storageBucket: "transit-matrix.appspot.com",
+		messagingSenderId: "777642611679"
+	};
+
+	firebase.initializeApp(config);	
+	
+	
+});
